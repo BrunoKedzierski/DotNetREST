782f471f5e59def9deb932b5591bcbfbead3d91f
Bruno Kozakowski-Kędzierski <s23719@pjwstk.edu.pl>
2022-03-24 11:45:17 +0100

    inint

---
+++ TaskUni/TaskUni/Controllers/StudentsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskUni.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        static List<String> list = new List<string>();
+
+
+
+
+
+        [HttpGet]
+        public IEnumerable<Student> GetStudentList()
+        {
+        }
+
+        [HttpGet("{IndexNum}")]
+        public Student GetStudentByIndex(int IndexNum) {
+
+
+            return IndexNum;
+        
+        
+        }
+
+    }
+
+
+}
